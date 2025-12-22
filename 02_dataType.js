7d8de84cfe7d353c28ed945f327fe8b4e1d6e283
Aaditya Sharma <sharmaa50045@gmail.com>
2025-12-22 17:46:14 +0000

    02-dataType

---
+++ 02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert (3+3) // we are using nodejs,not browser
+
+let name = "hitest"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value/mt value
+// undefined => 
+// symbol => unique
+
+// object
+  
+
+// console.log(typeof undefined); // undefined
+
+// console.log(typeof null); // object
+
+console.log(typeof "hello")
