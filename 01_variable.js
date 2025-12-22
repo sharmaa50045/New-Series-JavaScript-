d4e74faf15079c34c012751960a0541e4d226d8e
Aaditya Sharma <sharmaa50045@gmail.com>
2025-12-22 17:21:07 +0000

    01-variable

---
+++ 01_variable.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121212"
+acoountCity = "Bengaluru"
+
+
+
+console.log(accountId);
+
+ 
+// prefer not to use var//
+ // because of issue in block scope and function scope //
+
+
+console.log([accountId,accountEmail,accountPassword,acoountCity])
+
+
+
+
